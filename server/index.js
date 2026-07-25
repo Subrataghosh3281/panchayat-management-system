9446f80b253dc0464ce4709292b3a75e7a8fb0d3
Subrataghosh3281 <sg6090284@gmail.com>
2026-07-26 01:27:21 +0530

    Connect Express backend to MongoDB Atlas using Mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const PORT = 5000
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB Atlas successfully.'))
+  .catch((err) => console.error('MongoDB connection error:', err))
 
 app.get('/', (req, res) => {
   res.send('Panchayat Management System backend is running.')
